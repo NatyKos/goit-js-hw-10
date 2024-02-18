80c98af60c144815f22054ab25083d35a80f730e
NatyKos <145201378+NatyKos@users.noreply.github.com>
2024-02-18 19:48:33 +0000

    1-timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -2,14 +2,16 @@ import flatpickr from 'flatpickr';
 import iziToast from "izitoast";
 
 const btn = document.querySelector('button');
-
+btn.disabled = true;
+// календар в інпуті+алерт з бібліотеки при виборі минулої дати
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    const date = new Date();
+    const date = Date.now();
+   
     if (selectedDates[0] < date) {
       iziToast.show({
         iconUrl: '../img/icon.svg',
@@ -19,20 +21,37 @@ const options = {
         close: false,
         position: 'topRight',
       });
-      btn.disabled = true;
     } else {
       btn.disabled = false;
+      input.disabled = true;
+      timer;
     }
- console.log(selectedDates[0]);
-    },
+    console.log(selectedDates[0]);
+    
+  },
+  
 };
 
 const input = document.querySelector('#datetime-picker');
 flatpickr(input, options);
 
+// таймер
+class Timer{
+  constructor({ onTick }) {
+    this.onTick = onTick;
+    this.interval = null;
+  };
+  start() {
+    const startTime = new Date(input.value).getTime();
+    this.interval = setInterval(() => {
+      const curentTime = Date.now();
+      const delta = startTime - curentTime;
+      const time = this.convertMs(delta);
+      this.onTick(time);
+    },1000)
+  };
 
-
-function convertMs(ms) {
+  convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
@@ -40,17 +59,40 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = this.pad(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours =this.pad(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes =this.pad(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = this.pad(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
+};
+
+pad(value){
+  return String(value).padStart(2, '0')
+}
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+const dataDay = document.querySelector('.value[data-days]');
+const dataHour = document.querySelector('.value[data-hours]');
+const dataMinute = document.querySelector('.value[data-minutes]');
+const dataSecond = document.querySelector('.value[data-seconds]')
+
+function updateTimer({ days, hours, minutes, seconds }) {
+  dataDay.textContent = `${days}`;
+  dataHour.textContent = `${hours}`;
+  dataMinute.textContent = `${minutes}`;
+  dataSecond.textContent = `${seconds}`;
+}
+const timer = new Timer({
+  onTick: updateTimer
+  })
+
+
+btn.addEventListener('click', timer.start.bind(timer));
